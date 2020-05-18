f0342149f00233f3daf7c6e6daf92c128e678186
Rocco Bowling <rocco@chimerasw.com>
2020-05-18 18:01:16 -0400

    blank project import, frameworks for ios and mac, unit tests for ios and mac

---
+++ Cutlass/Cutlass-Shared/Cutlass.h
@@ -0,0 +1,19 @@
+//
+//  Cutlass.h
+//  Cutlass
+//
+//  Created by Rocco Bowling on 5/14/20.
+//  Copyright © 2020 Rocco Bowling. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Cutlass.
+FOUNDATION_EXPORT double CutlassVersionNumber;
+
+//! Project version string for Cutlass.
+FOUNDATION_EXPORT const unsigned char CutlassVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Cutlass/PublicHeader.h>
+
+
